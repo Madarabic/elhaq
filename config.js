7008f9629cbd23e101f6c9f74bcd3da0f83babdd
Madarabic <muh.restu5@gmail.com>
2026-09-09 16:49:59 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  */
 const CONFIG = {
     // Ganti dengan URL Deployment Web App Google Apps Script Anda yang aktif (/exec)
-    API_URL: "https://script.google.com/macros/s/AKfycbx_YOUR_DEPLOYMENT_ID_HERE/exec",
+    API_URL: "https://script.google.com/macros/s/AKfycbxYwuPjdbJtypEXn78R0TijQNVTcf1JksEKYEyycoDZiMeHp1qqUDJ5Fa_8hVxOxBXT/exec",
     APP_NAME: "HAQ SAUDI PREMIUM",
     VERSION: "1.1.0"
 };
