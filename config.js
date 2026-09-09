5d1f7e25888966eb05ede82a3d30cb178479fbff
Madarabic <muh.restu5@gmail.com>
2026-09-09 15:40:34 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * HAQ SAUDI PREMIUM - Configuration File
+ * Pisahkan URL Web App dan variabel global di sini agar mudah dikelola.
+ */
+const CONFIG = {
+    // URL Google Apps Script Web App Anda
+    API_URL: 'https://script.google.com/macros/s/AKfycbxYwuPjdbJtypEXn78R0TijQNVTcf1JksEKYEyycoDZiMeHp1qqUDJ5Fa_8hVxOxBXT/exec',
+    APP_VERSION: '1.0.0',
+    DEFAULT_LANGUAGE: 'id'
+};
